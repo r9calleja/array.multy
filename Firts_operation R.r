f2492cd829eaafc411471c704f3c21bf14fb42fb
r9calleja <140212219+r9calleja@users.noreply.github.com>
2023-10-26 16:59:19 -0300

    Repositorio.alc

---
+++ Firts_operation R.r
@@ -0,0 +1,4 @@
+quarter_1_sales  <- 35657.98
+quarter_2_sales  <- 43810.5
+midyear_sales  <- quarter_1_sales + quarter_2_sales
+yearend_sales  <- midyearsales * 2
---
+++ Untitled-1.r
@@ -0,0 +1,9 @@
+install.packages("palmerpenguins")
+
+install.packages("ggplot2")
+install.packages("tidyverse")
+
+
+quarter_1_sales  <- 35657.98
+quarter_2_sales  <- 43810.5
+midyear_sales  <- quarter_1_sales + quarter_2_sales
